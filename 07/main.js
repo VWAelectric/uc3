d99952ada8bf3b69b89166dd31412a37c52334fc
Clodoaldo Silva <cas_maquinas@hotmail.com>
2024-11-26 22:12:18 -0300

    estudo de funções em javaScript

---
+++ 07/main.js
@@ -0,0 +1,21 @@
+function saudacao(nome) {
+console.log('Olá,' + nome)
+}
+
+saudacao(' Marcos')
+
+function calculaODobro(valorOriginal) {
+    let dobro = valorOriginal * 2
+    return dobro
+}
+
+let resposta = calculaODobro(7)
+console.log(resposta)
+
+function multiplicar (a, b) {
+    return a * b
+}
+
+let resultado  = multiplicar(4, 5)
+console.log(resultado)
+
