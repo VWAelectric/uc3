fb82f49836f510a9a6c0f64053bed5e4f8d2c11c
clodoaldo <cas_maquinas@hotmail.com>
2025-01-27 19:21:11 -0300

    atualização da pasta projetos

--- projeto/loja_produtos/js/script.js
+++ projeto/loja_produtos/js/script.js
@@ -1,14 +1,42 @@
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll('.card');
 const btnRedefinir = document.getElementById('btnRedefinir');
+const btnComprar = document.getElementById('btnComprar');
+const resultado = document.getElementById('resultado');
+const telefoneDoWhatsapp = "5511948887050";
+const whatsappLink = document.getElementById("whatsappLink");
 
-cards.forEach(car => {
-    car.addEventListener("click", () => {
-        car.classList.toggle("selecionado");
-    });
-});
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        card.classList.toggle("selecionado")
+    })
+})
 
-cards.forEach(car => {
+cards.forEach(card => {
     btnRedefinir.addEventListener("click", () => {
-        car.classList.remove("selecionado");
+        card.style.display = "";
+        card.classList.remove("selecionado")
+        resultado.innerText = "";
+    })
+})
+
+btnComprar.addEventListener("click", () => {
+    let precoTotal = 0;
+    let listaProdutos = [];
+
+    cards.forEach(card => {
+        if(!card.classList.contains("selecionado")) {
+            card.style.display = "none";
+        } else {
+            listaProdutos.push(
+                card.querySelector("div:first-child").textContent + " R$ $" + card.dataset.preco);
+            precoTotal += parseFloat(card.dataset.preco);
+        }
     });
+    resultado.innerText = `Preço Total R$ ${precoTotal}`;
+
+    const mensagem = encodeURI(`Olá, gostaria de comprar os seguintes produtos:\n\n${listaProdutos.join("\n")}\n\n Preço Total R$ ${precoTotal}`);
+
+    whatsappLink.href = `https://wa.me/${telefoneDoWhatsapp}?text=${mensagem}`;
+
+
 });
